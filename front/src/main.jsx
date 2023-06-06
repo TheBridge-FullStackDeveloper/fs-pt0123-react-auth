7add860e3f9c87b39300fbc97c6631fb352c6efe
Riftland <fran.mscr@gmail.com>
2023-06-06 14:50:03 +0200

    feat: added routes

---
+++ front/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Provider from "./context/Provider";
+import Login from "./pages/Login";
+import Panel from "./pages/Panel";
+import { Switch, Route, Redirect } from "wouter";
+import Guard from "./components/Guard";
+
+const Main = () => {
+  return (
+    <Provider>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/">
+          <Guard component={Panel} />
+        </Route>
+        <Redirect to="/login" />
+      </Switch>
+    </Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
