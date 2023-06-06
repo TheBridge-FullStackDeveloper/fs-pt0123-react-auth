f4a69d02a6f8853e5f4439f884fe454590d6ec5a
Riftland <fran.mscr@gmail.com>
2023-06-06 14:49:11 +0200

    feat: added services with axios

---
+++ front/src/services/auth.js
@@ -0,0 +1,10 @@
+export const login = (client) => async (params) => {
+  try {
+    const { data } = await client.post("/auth/signin", params);
+    console.info("> login data: ", data);
+    return data;
+  } catch (error) {
+    console.info("> login error: ", error.message);
+    return { success: false };
+  }
+};
